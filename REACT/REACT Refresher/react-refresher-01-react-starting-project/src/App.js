98c57046f5172a970dcd8780b39a133e9a39d87c
Achint <achintkat12@gmail.com>
2023-08-09 14:35:10 +0530

    updated refresher react course

---
+++ REACT/REACT Refresher/react-refresher-01-react-starting-project/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import GoalList from './components/GoalList';
+import './App.css';
+
+const App = () => {
+  const courseGoals = [
+    {id: "cg1",text: "Finish the course"},
+    {id: "cg2",text: "Learn all about the course"},
+    {id: "cg3",text: "Help other students"},
+  ];
+  return <div className='course-goals'>
+    <h2>Course Goals</h2>
+   <GoalList goals= {courseGoals} />
+  </div>;
+};
+
+export default App;
---
+++ REACT/REACT Refresher/react-refresher-01-react-starting-project/src/components/GoalList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./GoalList.css";
+const GoalList = props => {
+return  (
+<ul className='goal-list'>
+
+
+</ul>);
+};
+
+export default GoalList; 

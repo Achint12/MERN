44f5613fd783a00dee0cfbd87c896049eb9f34b7
Achint <achintkat12@gmail.com>
2023-08-11 17:23:50 +0530

    Users list update

--- REACT/Main Course Project/react-frontend-01-starting-setup/src/User/Pages/User.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/User/Pages/User.js
@@ -1,5 +1,10 @@
 import React from "react";
+import UserList from "../Components/UserList";
 const users = () => {
-return <h2>User Works!</h2>;
+    const USERS= [{id: 'u1',name: 'Achint', image: 'https://unsplash.com/photos/a-city-street-with-tall-buildings-and-a-bridge-6c90hRXZs1o',places: 3}];
+
+    
+return <UserList items= {USERS}/>;
 };
+
 export default users;

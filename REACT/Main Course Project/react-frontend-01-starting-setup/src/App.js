3a8c95e3bec9a6a485dc800973dc61c25ea5cfd7
Achint <achintkat12@gmail.com>
2023-08-11 15:33:37 +0530

    Starting the main SPA app

--- REACT/Main Course Project/react-frontend-01-starting-setup/src/App.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
+import Users from './User/Pages/User';
+import NewPlace from './Places/Pages/Newplace';
 const App= () => {
-  return <Router>
-
-    </Router>;
-}
+  return (
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Users/>
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace/>
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+  );
+};
 
 export default App;
--- REACT/Main Course Project/react-frontend-01-starting-setup/src/User/Pages/User.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/User/Pages/User.js
@@ -1,5 +1,5 @@
 import React from "react";
 const users = () => {
-return <h2>User Works!</h2>
+return <h2>User Works!</h2>;
 };
 export default users;

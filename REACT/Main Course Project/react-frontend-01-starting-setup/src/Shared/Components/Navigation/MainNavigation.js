c3ca8e65287cae91b13a871c99c321fb93543222
Achint <achintkat12@gmail.com>
2023-08-13 11:01:03 +0530

    added backdrop and opening and closing of the menu

--- REACT/Main Course Project/react-frontend-01-starting-setup/src/Shared/Components/Navigation/MainNavigation.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/Shared/Components/Navigation/MainNavigation.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
+import Backdrop from "../../UIElements/Backdrop";
 import SideDrawer from "./SideDrawer";
 import MainHeader from "./Mainheader";
 import Navlinks from "./NavLinks";
 import './MainNavigation.css';
 const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer=() =>{
+        setDrawerIsOpen(true);
+    };
+    const closeDrawer =() => {
+        setDrawerIsOpen(false);
+    };
+
     return (
     <React.Fragment>
-    <SideDrawer>
+        {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+    { drawerIsOpen && (<SideDrawer>
         <nav className="main-navigation__drawer-nav">
             <Navlinks/>
         </nav>
-    </SideDrawer>   
+    </SideDrawer> )} 
     <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
             <span/>
             <span/>
             <span/>

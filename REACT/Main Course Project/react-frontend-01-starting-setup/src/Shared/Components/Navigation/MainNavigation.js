5d0164039ecda5c031253b1febe0ec97d448b9fb
Achint <achintkat12@gmail.com>
2023-08-13 10:34:31 +0530

    updated Mobile view

--- REACT/Main Course Project/react-frontend-01-starting-setup/src/Shared/Components/Navigation/MainNavigation.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/Shared/Components/Navigation/MainNavigation.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
+import SideDrawer from "./SideDrawer";
 import MainHeader from "./Mainheader";
 import Navlinks from "./NavLinks";
 import './MainNavigation.css';
 const MainNavigation = props => {
     return (
+    <React.Fragment>
+    <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+            <Navlinks/>
+        </nav>
+    </SideDrawer>   
     <MainHeader>
         <button className="main-navigation__menu-btn">
             <span/>
@@ -15,13 +22,15 @@ const MainNavigation = props => {
         <h1 className="main-navigation__title">
            <Link to='/'> Your places </Link>
         </h1>
-        <nav>
+        <nav className= "main-navigation__header-nav"> 
         <Navlinks>
-            
+
         </Navlinks>
         </nav>
     </MainHeader>
+    </React.Fragment>
     ); 
 };
 
 export default MainNavigation;
+//in react one must have only one root jsx element per component one can nest as many elements one wants. hence to use that one has to use <React.fragment> element.

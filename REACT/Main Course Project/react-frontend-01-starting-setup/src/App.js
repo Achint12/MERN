1e09f78c54e6723cc63d3f74d6f7042b96b60ec7
Achint <achintkat12@gmail.com>
2023-08-13 10:07:19 +0530

    updated header file

--- REACT/Main Course Project/react-frontend-01-starting-setup/src/App.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
+import MainHeader from './Shared/Components/Navigation/Mainheader';
 import Users from './User/Pages/User';
 import NewPlace from './Places/Pages/Newplace';
+import MainNavigation from './Shared/Components/Navigation/MainNavigation';
 const App= () => {
   return (
   <Router>
+    <MainNavigation/>
+    <main>
     <Switch>
       <Route path="/" exact>
         <Users/>
@@ -15,6 +19,7 @@ const App= () => {
       </Route>
       <Redirect to="/" />
     </Switch>
+    </main>
   </Router>
   );
 };

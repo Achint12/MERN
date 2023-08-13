d2e712d06c7f90baf6e232da5b98c2fba6488d8b
Achint <achintkat12@gmail.com>
2023-08-13 21:01:24 +0530

    added map button functionality

--- REACT/Main Course Project/react-frontend-01-starting-setup/src/Shared/Components/UIElements/Map.js
+++ REACT/Main Course Project/react-frontend-01-starting-setup/src/Shared/Components/UIElements/Map.js
@@ -1,11 +1,14 @@
-import React from "react";
-
+import React, {useRef} from "react";
+//useref can be used to create references which can create so called references which
 import './Map.css'
 
 const Map= props => {
+    const map =new window.google.maps.Map();
+
 return <div className={'map ${props.className}'}
-style={props.style}
-></div>
+style={props.style}>
+
+</div>;
 };
 
 export default Map;

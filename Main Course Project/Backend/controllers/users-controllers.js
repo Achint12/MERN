36efaa4449b3dab7beed46a23162dcb95ed76383
Achint <achintkat12@gmail.com>
2023-08-18 03:02:08 +0530

    added user support with user routes

---
+++ Main Course Project/Backend/controllers/users-controllers.js
@@ -0,0 +1,48 @@
+const uuid= require('uuid/v4');
+
+const HttpError = require('../models/http-error');
+
+const DUMMY_USER=[
+    {
+        id:'u1',
+        name:"Achint",
+        email:"email@email.com",
+        password:'testers'
+    }
+];
+
+const getUsers = (req, res, next )=>{
+res.json({users: DUMMY_USER});
+};
+
+const signup= (req, res, next)=>{
+const {name, email, password}= req.body;
+const hasUser= DUMMY_USER.find(u=> u.email === email);
+if(hasUser){
+    throw new HttpError("could not create user email already exist ", 422)
+}
+
+const creadedUser={
+    id: uuid(),
+    name,
+    email,
+    password
+    
+};
+DUMMY_USER.push(creadedUser);
+res.status(201).json({user: creadedUser});
+};
+const login= (req, res, next)=>{
+    const{email,password}= req.body;
+    
+    const identifiedUser = DUMMY_USER.find(u=> u.email=== email);
+    if (!identifiedUser|| identifiedUser.password!== password){
+        throw new HttpError("could not identify user", 401)
+    }
+res.json({message:'Logged in !'});
+
+};
+
+exports.getUsers= getUsers;
+exports.signup= signup;
+exports.login = login;

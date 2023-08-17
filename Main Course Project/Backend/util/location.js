a6dece09e9dc597cd8ee7c1623806792c3913e52
Achint <achintkat12@gmail.com>
2023-08-18 04:31:26 +0530

    added google gelocation

---
+++ Main Course Project/Backend/util/location.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const HttpError = require('../models/http-error');
+const API_KEY ='AIzaSyDHAo0fzEhnl1gGYiD3D6qGG-g-rh2MsC4';
+
+async function getCoordsForAddress(address){
+ const response =await    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+
+ const data = response.data;
+
+ if(!data|| data.status === 'ZERO_RESULTS'){
+    const error = new HttpError('could not find the location for the specified address',422);
+    throw error;
+ }
+ const coordinates = data.results[0].geometry.location;
+
+ return coordinates;
+};
+
+module.exports = getCoordsForAddress;
